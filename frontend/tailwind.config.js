f854310a2f179e0a71dd04c7848c78ff381e1191
Haruka <hfujii1013@gmail.com>
2023-12-14 01:05:10 -0800

    feat: set up tailwind

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/tailwind-datepicker-react/dist/**/*.js",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        xs: "480px",
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+      },
+    },
+  },
+  plugins: [],
+};
