da50ba5285a5f0befe50941a79bb0ec3e224ea6e
Haruka <hfujii1013@gmail.com>
2023-12-14 00:16:06 -0800

    feat: add index

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const methodOverride = require("method-override");
+const cors = require("cors");
+
+const membersRouter = require("./src/routers/members.router");
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(methodOverride("_method"));
+
+// API Endpoints
+app.use("/api/members", membersRouter);
+
+app.use((req, res) => {
+  res.status(404).json({ error: "Not Found" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
