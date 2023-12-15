5e8f532a8d99888dcb02ae1848db2a891e79a72d
Haruka <hfujii1013@gmail.com>
2023-12-15 01:30:54 -0800

    fix: fix schema

--- backend/src/service/postgres.js
+++ backend/src/service/postgres.js
@@ -15,12 +15,16 @@ const pool = new Pool({
 // Function to check if the Members table exists and seed the database
 const checkTableExists = async () => {
   try {
-    const data = await pool.query(`SELECT to_regclass('Members')`);
-    if (data.rows.length === 0) {
-      console.log(`Table 'Members' does not exist`);
+    const tableCheckQuery =
+      "SELECT EXISTS (SELECT FROM pg_tables WHERE schemaname = 'public' AND tablename = 'members')";
+    const tableExistsResult = await pool.query(tableCheckQuery);
+    const tableExists = tableExistsResult.rows[0].exists;
+
+    if (!tableExists) {
+      console.log(`Table 'members' does not exist`);
       await seedDB();
     } else {
-      console.log(`Table 'Members' exists`);
+      console.log(`Table 'members' exists`);
     }
   } catch (err) {
     console.error(err);
@@ -30,28 +34,28 @@ const checkTableExists = async () => {
 // Function to create the Members table and seed it with initial data
 const seedDB = async () => {
   try {
-    await pool.query(`DROP TABLE IF EXISTS Members`);
+    await pool.query(`DROP TABLE IF EXISTS members`);
 
     await pool.query(
-      `CREATE TABLE Members (
-        Member_ID SERIAL PRIMARY KEY,
-        FirstName VARCHAR(100) NOT NULL,
-        LastName VARCHAR(100) NOT NULL,
-        Email VARCHAR(255) UNIQUE,
-        PhoneNum VARCHAR(15),
-        Role VARCHAR(10) NOT NULL DEFAULT 'Regular' CHECK (Role IN ('Regular', 'Admin'))
+      `CREATE TABLE members (
+        member_id SERIAL PRIMARY KEY,
+        first_name VARCHAR(100) NOT NULL,
+        last_name VARCHAR(100) NOT NULL,
+        email VARCHAR(255) UNIQUE,
+        phone_num VARCHAR(15),
+        role VARCHAR(10) NOT NULL DEFAULT 'Regular' CHECK (role IN ('Regular', 'Admin'))
       )`
     );
-    console.log("Successful creation of the 'Members' table");
+    console.log("Successful creation of the 'members' table");
 
     await pool.query(
-      `INSERT INTO Members (FirstName, LastName, Email, PhoneNum, Role) VALUES
+      `INSERT INTO members (first_name, last_name, email, phone_num, role) VALUES
       ('Haruka', 'Fujii', 'haruka.fujii@gmail.com', '1234567890', 'Regular'),
       ('Admin', 'Ito', 'admin.ito@gmail.com', '0987654321', 'Admin')`
     );
     console.log("Successful creation of initial members");
   } catch (err) {
-    console.error(err.message);
+    console.error("Error in seedDB:", err.message);
   }
 };
 
@@ -60,3 +64,66 @@ checkTableExists();
 
 // Export the pool for use in other parts of your application
 module.exports = pool;
+
+// const { Pool } = require("pg");
+
+// // Setup the connection pool to the PostgreSQL database
+// const pool = new Pool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: process.env.DB_NAME,
+//   port: process.env.DB_PORT,
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+// });
+
+// // Function to check if the Members table exists and seed the database
+// const checkTableExists = async () => {
+//   try {
+//     const data = await pool.query(`SELECT to_regclass('Members')`);
+//     if (data.rows.length === 0) {
+//       console.log(`Table 'Members' does not exist`);
+//       await seedDB();
+//     } else {
+//       console.log(`Table 'Members' exists`);
+//     }
+//   } catch (err) {
+//     console.error(err);
+//   }
+// };
+
+// // Function to create the Members table and seed it with initial data
+// const seedDB = async () => {
+//   try {
+//     await pool.query(`DROP TABLE IF EXISTS Members`);
+
+//     await pool.query(
+//       `CREATE TABLE Members (
+//         Member_ID SERIAL PRIMARY KEY,
+//         FirstName VARCHAR(100) NOT NULL,
+//         LastName VARCHAR(100) NOT NULL,
+//         Email VARCHAR(255) UNIQUE,
+//         PhoneNum VARCHAR(15),
+//         Role VARCHAR(10) NOT NULL DEFAULT 'Regular' CHECK (Role IN ('Regular', 'Admin'))
+//       )`
+//     );
+//     console.log("Successful creation of the 'Members' table");
+
+//     await pool.query(
+//       `INSERT INTO Members (FirstName, LastName, Email, PhoneNum, Role) VALUES
+//       ('Haruka', 'Fujii', 'haruka.fujii@gmail.com', '1234567890', 'Regular'),
+//       ('Admin', 'Ito', 'admin.ito@gmail.com', '0987654321', 'Admin')`
+//     );
+//     console.log("Successful creation of initial members");
+//   } catch (err) {
+//     console.error(err.message);
+//   }
+// };
+
+// // Call the function to check if the table exists and seed the database
+// checkTableExists();
+
+// // Export the pool for use in other parts of your application
+// module.exports = pool;
