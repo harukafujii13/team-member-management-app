c325fe644ba7b10df4c219bf229919e6d21de78c
Haruka <hfujii1013@gmail.com>
2023-12-15 01:06:47 -0800

    feat: add AddPage

--- frontend/src/routes/AddPage.jsx
+++ frontend/src/routes/AddPage.jsx
@@ -1,5 +1,55 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
+
 const AddPage = () => {
-  return <div>Add Page</div>;
+  const [memberData, setMemberData] = useState({ role: "Regular" });
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .post("http://localhost:8000/api/members/create", memberData)
+      .then(() => navigate("/"))
+      .catch((error) => console.error("Error adding member:", error));
+  };
+
+  const handleChange = (event) => {
+    setMemberData({ ...memberData, [event.target.name]: event.target.value });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        name="firstName"
+        onChange={handleChange}
+        placeholder="First Name"
+      />
+      <input
+        name="lastName"
+        onChange={handleChange}
+        placeholder="Last Name"
+      />
+      <input
+        name="email"
+        onChange={handleChange}
+        placeholder="Email"
+      />
+      <input
+        name="phoneNum"
+        onChange={handleChange}
+        placeholder="Phone Number"
+      />
+      <select
+        name="role"
+        onChange={handleChange}
+        defaultValue="Regular">
+        <option value="Regular">Regular</option>
+        <option value="Admin">Admin</option>
+      </select>
+      <button type="submit">Save</button>
+    </form>
+  );
 };
 
 export default AddPage;
