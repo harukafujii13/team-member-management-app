11dc7e2dd76fc308459847d3a0c2f7418eb1ec1b
Haruka <hfujii1013@gmail.com>
2023-12-17 20:17:59 -0800

    fix: fix API_BASE_URL

--- frontend/src/api/service/memberService.js
+++ frontend/src/api/service/memberService.js
@@ -1,19 +1,20 @@
 import axios from "axios";
 import { toast } from "react-toastify";
 
-const API_BASE_URL = `${import.meta.env.VITE_BACKEND_URL}/api/v1/`;
-
 export const fetchAllMembers = () => {
-  return axios.get(`${API_BASE_URL}/all`);
+  return axios.get(`${process.env.API_BASE_URL}/all`);
 };
 
 export const fetchMemberById = (id) => {
-  return axios.get(`${API_BASE_URL}/edit/${id}`);
+  return axios.get(`${process.env.API_BASE_URL}/edit/${id}`);
 };
 
 export const createMember = async (memberData) => {
   try {
-    const response = await axios.post(`${API_BASE_URL}/create`, memberData);
+    const response = await axios.post(
+      `${process.env.API_BASE_URL}/create`,
+      memberData
+    );
     toast.success("Member created successfully!");
     return response;
   } catch (error) {
@@ -27,9 +28,9 @@ export const createMember = async (memberData) => {
 };
 
 export const updateMember = (id, memberData) => {
-  return axios.post(`${API_BASE_URL}/edit/${id}`, memberData);
+  return axios.post(`${process.env.API_BASE_URL}/edit/${id}`, memberData);
 };
 
 export const deleteMember = (id) => {
-  return axios.delete(`${API_BASE_URL}/delete/${id}`);
+  return axios.delete(`${process.env.API_BASE_URL}/delete/${id}`);
 };
